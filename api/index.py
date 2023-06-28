a5e841c3b827ed4c21d9891638128a4b2f86e4bd
YashNahata <yashnahata16@gmail.com>
2023-06-29 02:12:57 +0530

    Fetch block number by date added

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-import requests
+import requests, time, datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -21,5 +21,32 @@ def gas_used():
             })
             res = jsonify(obj)
         return jsonify(obj)
+    except requests.exceptions.RequestException as e:
+        return f"Error: {str(e)}"
+
+@app.route("/api/block-numbers", methods = ['POST'])
+def block_numbers():
+    date = request.json['date']
+    year = int(date[0:4])
+    month = int(date[5:7])
+    day = int(date[8:])
+    unix_timestamp_1 = int(time.mktime(datetime.datetime(year, month, day, 0, 0).timetuple()))
+    unix_timestamp_2 = int(time.mktime(datetime.datetime(year, month, day, 23, 59, 59).timetuple()))
+    url1 = f"https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp={unix_timestamp_1}&closest=before&apikey=CZE5M66GGG4VEWACQKJZ56JNJ7G44YMU2Z"
+    url2 = f"https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp={unix_timestamp_2}&closest=before&apikey=CZE5M66GGG4VEWACQKJZ56JNJ7G44YMU2Z"
+    try:
+        obj = {}
+        response1 = requests.get(url1)
+        response1.raise_for_status()
+        data1 = response1.json()
+        obj["first"] = data1["result"]
+
+        response2 = requests.get(url2)
+        response2.raise_for_status()
+        data2 = response2.json()
+        obj["last"] = data2["result"]
+
+        res = jsonify(obj)
+        return res
     except requests.exceptions.RequestException as e:
         return f"Error: {str(e)}"
--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,81 @@
 "use client";
 import { createChart } from "lightweight-charts";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Chart() {
+  const [firstBlockNumber, setFirstBlockNumber] = useState(0);
+  const [lastBlockNumber, setLastBlockNumber] = useState(0);
   const chartRef = useRef(null);
-  fetch("http://127.0.0.1:5328/api/gas-used")
-    .then((res) => res.json())
-    .then((data) => {
-      if (chartRef.current && data) {
-        data = data.reverse();
-        const chart = createChart(chartRef.current, {
-          width: 600,
-          height: 300,
-        });
-        const lineSeries = chart.addLineSeries();
 
-        chart.add;
+  useEffect(() => {
+    fetch("http://127.0.0.1:5328/api/gas-used")
+      .then((res) => res.json())
+      .then((data) => {
+        if (chartRef.current && data) {
+          data = data.reverse();
+          const chart = createChart(chartRef.current, {
+            width: 600,
+            height: 300,
+          });
+          const lineSeries = chart.addLineSeries();
 
-        lineSeries.setData(data);
-        chart.applyOptions({
-          timeScale: {
-            timeVisible: true,
-            secondsVisible: false,
-          },
-          priceScale: {
-            position: "right",
-          },
-        });
-      }
-    });
+          chart.add;
+
+          lineSeries.setData(data);
+          chart.applyOptions({
+            timeScale: {
+              timeVisible: true,
+              secondsVisible: false,
+            },
+            priceScale: {
+              position: "right",
+            },
+          });
+        }
+      });
+  }, []);
+
+  const getBlockNumber = (e) => {
+    e.preventDefault();
+    let obj = {
+      date: document.getElementById("day").value,
+    };
+    fetch("http://127.0.0.1:5328/api/block-numbers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setFirstBlockNumber(data.first);
+        setLastBlockNumber(data.last);
+      });
+  };
 
   return (
     <>
-      <legend>Ethereum Daily Gas Used Chart</legend>
-      <div ref={chartRef} />
+      <legend className="p-2">Ethereum Daily Gas Used Chart</legend>
+      <div className="p-2" ref={chartRef} />
+      <br />
+      <form id="form" className="p-2" method="post" onSubmit={getBlockNumber}>
+        <label htmlFor="day">Enter the day: </label>
+        <input type="date" name="day" id="day" className="text-black" />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white ml-2 font-bold py-2 px-4 rounded"
+        >
+          Submit
+        </button>
+      </form>
+      <br />
+      <p className="p-2">
+        The first block for the above date is : {firstBlockNumber}
+      </p>
+      <p className="p-2">
+        The last block for the above date is : {lastBlockNumber}
+      </p>
     </>
   );
 }
